29318060d762d4fc9e31c6ec48e7daef49b30094
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2023-06-05 21:59:52 +0300

    change solution

--- src/main/java/org/example/Сalculator.java
+++ src/main/java/org/example/Сalculator.java
@@ -3,48 +3,65 @@ package org.example;
 import java.util.Scanner;
 
 public class Сalculator {
+    private static final String ALLOW_OPERATORS = "+-*/";
 
     public static void main(String[] args) {
-        int firstNum = 0;
-        int secondNum = 0;
-        String operation = null;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter first number");
-        while (true) {
-            try {
-                firstNum = scanner.nextInt();
-                break;
-            } catch (RuntimeException ex) {
-                System.out.println("Please enter correct value");
-                scanner.nextLine();
-            }
-        }
-        System.out.println("Please enter second number");
-        while (true) {
-            try {
-                secondNum = scanner.nextInt();
-                break;
-            } catch (RuntimeException ex) {
-                System.out.println("Please enter correct value");
-                scanner.nextLine();
-            }
-
-        }
-        System.out.println("Please enter operation which you do ( +, -, /, *)");
-        while (true) {
-            operation = scanner.next();
-            switch (operation) {
-                case "+" -> System.out.print(firstNum + secondNum);
-                case "-" -> System.out.print(firstNum - secondNum);
-                case "/" -> System.out.print(firstNum / secondNum);
-                case "*" -> System.out.print(firstNum * secondNum);
-                default -> {
-                    System.out.print("Invalid value, please enter correct value\n");
-                    scanner.nextLine();
-                }
-            }
-
-        }
+        calculate();
 
     }
+
+    public static void calculate() {
+        System.out.println("Please enter following expression [num1 operation num2] where operations is +-*/ and separator must be space ");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String[] operators = scanner.nextLine().split(" ");
+            if("Q".contains(operators[0])){
+                break;
+            }
+            if (invalid(operators))
+                continue;
+
+            int num1 = Integer.parseInt(operators[0]);
+            int num2 = Integer.parseInt(operators[2]);
+
+            switch (operators[1]) {
+                case "+" -> System.out.println(num1 + num2);
+                case "-" -> System.out.println(num1 - num2);
+                case "/" -> System.out.println(num1 / num2);
+                case "*" -> System.out.println(num1 * num2);
+            }
+        }
+    }
+
+    private static boolean invalid(String[] operators) {
+        // length of operators must be 3
+        if (operators.length != 3) {
+            System.out.println("must 3 elements");
+            return true;
+        }
+        // parse two nums
+        if (parseStringToInteger(operators[0]) == null || parseStringToInteger(operators[2]) == null) {
+            System.out.println("one of the nums is invalid");
+            return true;
+        }
+        //check allowed operations
+        if (!ALLOW_OPERATORS.contains(operators[1])) {
+            System.out.println("contains invalid operations allowed only +-*/");
+            return true;
+        }
+        return false;
+    }
+
+    private static Integer parseStringToInteger(String num) {
+        Integer result = null;
+        try {
+            result = Integer.valueOf(num);
+        } catch (RuntimeException ex) {
+            //System.err.println("Please enter correct value");
+        }
+        return result;
+    }
+
+
 }
